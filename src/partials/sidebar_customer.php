ae84b31ca5fcf4a16e19fa8c4e498f65a9cdbd1c
Xiaofan Li <xli2024@terpmail.umd.edu>
2026-07-02 13:09:42 -0400

    Added New Icon

--- src/partials/sidebar_customer.php
+++ src/partials/sidebar_customer.php
@@ -18,7 +18,7 @@
   <!-- Sidebar Header -->
   <header class="sidebar-header">
 
-    <div class="auth-card__logo">P</div>
+    <div class="auth-card__logo"><?php include 'icon.svg'; ?></div>
     <button class="sidebar-toggle">
       <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
         <polyline points="15 18 9 12 15 6"></polyline>
